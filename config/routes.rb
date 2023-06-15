70ae3995ba43c5509add240a570debdc1a1eece0
sipovalovdmitro <sipovalovdmitro@gmail.com>
2023-06-15 22:04:10 +0000

    fixed api router

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tickets
   get 'api/index'
+  post 'api/create_ticket_with_excavator'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
